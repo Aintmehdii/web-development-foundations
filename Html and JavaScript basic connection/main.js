2e43579fa542b5527c247ef9c7783b2384f988ee
Mahdi <omohammadi376@gmail.com>
2026-09-02 12:54:18 +0330

    Initialize main.js with input element selectors

---
+++ Html and JavaScript basic connection/main.js
@@ -0,0 +1,6 @@
+const usernameinput = document.querySelector("#getusername")
+const levelinput = document.querySelector("#getlevel")
+const countryinput = document.querySelector("#getcountry")
+const loginbtn = document.querySelector("#Loginbtn")
+const displayloginresult = document.querySelector("#displayloginresult")
+
